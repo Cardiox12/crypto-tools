bc7758bdb9a7dea7eb587f06dcce41ae7cf9a325
Toto <cardiox1212@hotmail.fr>
2018-04-15 13:46:43 +0200

    Upload

--- crypto_playground.py
+++ crypto_playground.py
@@ -1,19 +1,5 @@
 # Solving challenge here
 
+# Import crypto tools
 from crypto_tools.substitute import *
 from crypto_tools.analysis import *
-
-lines = """tm bcsv qolfp
-f'dmvd xuhm exl tgak
-hlrkiv sydg hxm
-qiswzzwf qrf oqdueqe
-dpae resd wndo
-liva bu vgtokx sjzk
-hmb rqch fqwbg
-fmmft seront sntsdr pmsecq""".split("\n")
-
-for line in lines:
-    print("SOLVING LINE : {} ".format(line))
-    for solve in brute_force(line):
-        print(solve)
-    print("_"*100)
--- crypto_tools/substitute.py
+++ crypto_tools/substitute.py
@@ -5,15 +5,13 @@ def decode(message, shift):
   final_message = []
 
   for ascii in ascii_codes:
-    if ascii != 32 and ascii != 10:
+    if (chr(ascii)).isalpha():
       if ascii - shift < 97:
         final_message.append((122 - 97 % (ascii - shift)) + 1)
       else:
         final_message.append(ascii - shift)
-    elif ascii == 32:
-        final_message.append(32)
-    elif ascii == 10:
-        final_message.append(10)
+    else:
+        final_message.append(ascii)
 
   return "".join([chr(x) for x in final_message])
 
@@ -23,15 +21,13 @@ def encode(message, shift):
   final_message = []
 
   for ascii in ascii_codes:
-    if ascii != 32 and ascii != 10:
+    if (chr(ascii)).isalpha():
       if ascii + shift > 122:
         final_message.append(96 + (ascii + shift) % 122)
       else:
         final_message.append(ascii + shift)
-    elif ascii == 32:
-        final_message.append(32)
-    elif ascii == 10:
-        final_message.append(10)
+    else:
+        final_message.append(ascii)
 
   return "".join([chr(x) for x in final_message])
 
@@ -39,7 +35,7 @@ def encode(message, shift):
 def brute_force(message):
   messages = []
 
-  for i in range(25):
+  for i in range(26):
     messages.append(decode(message, i))
 
   return messages
@@ -50,15 +46,13 @@ def decode_simple(message, shift):
   final_message = []
 
   for ascii in ascii_codes:
-    if ascii != 32 and ascii != 10:
+    if (chr(ascii)).isalpha():
       if ascii - shift < 0:
         final_message.append(256 - (ascii - shift))
       else:
         final_message.append(ascii - shift)
-    elif ascii == 32:
-        final_message.append(32)
-    elif ascii == 10:
-        final_message.append(10)
+    else:
+        final_message.append(ascii)
 
 
   return "".join([chr(x) for x in final_message])
@@ -71,8 +65,3 @@ def brute_force_simple(message):
     messages.append(decode_simple(message, i))
 
   return messages
-
-
-if __name__ == "__main__":
-    message = brute_force("hmb rqch fqwbg")
-    print(message)
