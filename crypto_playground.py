468703ff3be2504c0ef62b8558a78c4d5abc8367
Toto <cardiox1212@hotmail.fr>
2018-04-29 19:51:49 +0200

    upload to github

--- crypto_playground.py
+++ crypto_playground.py
@@ -1,5 +1,23 @@
 # Solving challenge here
 
 # Import crypto tools
-from crypto_tools.substitute import *
-from crypto_tools.analysis import *
+# from crypto_tools.substitute import *
+# from crypto_tools.analysis import *
+
+import sys
+import time
+import requests
+
+# Coding here 
+
+from random import randint
+
+# def xrange(message):
+#   message = list(message)
+#   for i in range(len(message)):
+#     yield message.pop(randint(0, len(message) - 1))
+
+
+
+
+
